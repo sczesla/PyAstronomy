ffa01553aa605709a2a9ee8076c5778648067eb1
stefan <stefan.czesla@hs.uni-hamburg.de>
2013-02-17 15:05:25 +0100

    Added example to sanity check

--- src/pyasl/resBased/exampleSanity.py
+++ src/pyasl/resBased/exampleSanity.py
@@ -36,3 +36,32 @@ class SanityOfBaraffe98Tracks(unittest.TestCase):
     
     # Finally, show the plot
 #    plt.show()
+
+class SanityOfNasaExoplanetArchive(unittest.TestCase):
+  
+  def setUp(self):
+    pass
+  
+  def tearDown(self):
+    pass
+  
+  def sanity_example(self):
+    from PyAstronomy import pyasl
+    import matplotlib.pylab as plt
+    
+    nexa = pyasl.NasaExoplanetArchive()
+    
+    # See what information is available
+    cols = nexa.availableColumns()
+    print
+    
+    # Get all information for planet 'wasp-12 b'
+    # By default, the search is case-insensitive
+    print "Entry of Wasp-12 b"
+    print nexa.selectByPlanetName("Wasp-12 b")
+    
+    print
+    # Get all data and plot ra vs. dec
+    dat = nexa.getAllData()
+    plt.plot(dat.ra, dat.dec, 'b.')
+#    plt.show()
