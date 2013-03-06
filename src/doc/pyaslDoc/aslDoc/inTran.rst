ec9aed376c40d26a62de566cbf2c82a5e133da9a
Stefan <stefan.czesla@hs.uni-hamburg.de>
2013-03-06 13:06:55 +0100

    Providing examples for inTransit

--- src/doc/pyaslDoc/aslDoc/inTran.rst
+++ src/doc/pyaslDoc/aslDoc/inTran.rst
@@ -3,3 +3,50 @@ Check whether time falls in transit window
 
 .. currentmodule:: PyAstronomy.pyasl
 .. autofunction:: isInTransit
+
+
+Example: Check individual point in time
+----------------------------------------
+
+::
+
+  from PyAstronomy import pyasl
+  
+  # Time of interest
+  time = 2476357.756234
+  # Define some (arbitrary) transit parameters
+  T0 = 2475123.01245
+  period = 3.4789112
+  duration = 2.2/24.0
+  
+  # Check whether the time is in-transit
+  print "Time is within transit? ",
+  if not pyasl.isInTransit(time, T0, period, duration/2.0):
+    print "No"
+  else:
+    print "Yes"
+
+
+Example: Checking a series of times
+------------------------------------
+    
+::
+
+  from PyAstronomy import pyasl
+  import numpy as np
+  
+  # Times of interest
+  times = 2476357.756234 + np.linspace(0.0, 5.0, 300)
+  # Define some (arbitrary) transit parameters
+  T0 = 2475123.01245
+  period = 3.4789112
+  duration = 2.2/24.0
+  
+  # Check whether the time is in-transit
+  print "Indices if time points within transit: ",
+  print pyasl.isInTransit(times, T0, period, duration/2.0)
+  
+  print
+  print "For each time point, a flag indicating whether it"
+  print "is in- or off-transit:"
+  print pyasl.isInTransit(times, T0, period, duration/2.0, boolOutput=True)
--- src/pyasl/asl/sanityTest.py
+++ src/pyasl/asl/sanityTest.py
@@ -1185,3 +1185,46 @@ class SanityOfTransit:
     # Estimate the duration of Earth's transit
     td = pyasl.transitDuration(1.0, reJ, 1.0, 90.0, 365.0)
     print "The transit of Earth lasts about: %5.3f days" % td
+  
+  def sanity_inTransitExample_1(self):
+    """
+      inTransit---individual point in time
+    """
+    from PyAstronomy import pyasl
+
+    # Time of interest
+    time = 2476357.756234
+    # Define some (arbitrary) transit parameters
+    T0 = 2475123.01245
+    period = 3.4789112
+    duration = 2.2/24.0
+    
+    # Check whether the time is in-transit
+    print "Time is within transit? ",
+    if not pyasl.isInTransit(time, T0, period, duration/2.0):
+      print "No"
+    else:
+      print "Yes"
+  
+  def sanity_inTransitExample_2(self):
+    """
+      inTransit---Series of points in time
+    """
+    from PyAstronomy import pyasl
+    import numpy as np
+    
+    # Times of interest
+    times = 2476357.756234 + np.linspace(0.0, 5.0, 300)
+    # Define some (arbitrary) transit parameters
+    T0 = 2475123.01245
+    period = 3.4789112
+    duration = 2.2/24.0
+    
+    # Check whether the time is in-transit
+    print "Indices if time points within transit: ",
+    print pyasl.isInTransit(times, T0, period, duration/2.0)
+    
+    print
+    print "For each time point, a flag indicating whether it"
+    print "is in- or off-transit:"
+    print pyasl.isInTransit(times, T0, period, duration/2.0, boolOutput=True)
