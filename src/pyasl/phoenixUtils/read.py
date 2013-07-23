fe8d741a66019315e219a7337f23de776f02d8d6
Stefan <stefan.czesla@hs.uni-hamburg.de>
2013-07-23 15:10:53 +0200

    Tool for reading tables with numbers in d-floating point format

--- src/pyasl/phoenixUtils/read.py
+++ src/pyasl/phoenixUtils/read.py
@@ -55,6 +55,47 @@ def readUnit7(fn, minwl=None, maxwl=None):
   return result
 
 
+def readDTable(fn, sort=None):
+  """
+    Read a table with numbers in d-format.
+    
+    In d-format, a number, e.g., looks 1.3d-3.
+    
+    Parameters
+    ----------
+    fn : string
+        Filename.
+    sort : int, optional
+        If given, the table will be sorted with respect
+        to that column. The first columns is 0.
+    
+    Returns
+    -------
+    table : array
+        An array with as many columns and rows as the table
+        read from file.
+  """
+  if not os.path.isfile(fn):
+    raise(PE.PyAValError("There is no file named: " + str(fn)))
+  # Check whether it is a gzipped file
+  r = re.match(".*\.gz", fn)
+  if r is not None:
+    f = gzip.open(fn, 'r')
+  else:
+    f = open(fn, 'r')
+  lines = f.readlines()
+  f.close()
+  # Find number of columns
+  ncol = len(lines[0].split())
+  result = np.zeros((len(lines), ncol))
+  for i in xrange(len(lines)):
+    result[i,::] = np.array(lines[i].replace('D','E').split(), dtype=np.float)
+  if sort is not None:
+    # Sort with respect to column
+    result = result[np.argsort(result[::,sort]),::]
+  return result
+
+
 def decomposeFilename(fn):
   """
     Decompose PHOENIX filename.
