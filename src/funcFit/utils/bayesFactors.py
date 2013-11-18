f35af9046e0ac652791d4ff0f473a9390b5c3317
Stefan <stefan.czesla@hs.uni-hamburg.de>
2013-11-18 17:22:14 +0100

    Extended docu for 'bayesFactors'

--- src/funcFit/utils/bayesFactors.py
+++ src/funcFit/utils/bayesFactors.py
@@ -1,18 +1,24 @@
 import numpy
 
-def bayesFactors(model1,model2):
+def bayesFactors(model1, model2):
   """
-    Computes the Bayes factor for two competing models
-    according to the prescription by Newton & Raftery 1994.
+    Computes the Bayes factor for two competing models.
+    
+    The computation is based on Newton & Raftery 1994 (Eq. 13).
     
     Parameters
     ----------
     model1 : PyAstronomy.funcFit.TraceAnalysis instance
+        TraceAnalysis for the first model.
     model2 : PyAstronomy.funcFit.TraceAnalysis instance
+        TraceAnalysis for the second model.
     
     Returns
     -------
-    The Bayes factor BF (neither log10(BF) nor ln(BF)
+    BF : float
+        The Bayes factor BF (neither log10(BF) nor ln(BF)).
+        Note that a small value means that the first model
+        is favored, i.e., BF=p(M2)/p(M1).
     
   """
   import pymc
@@ -20,6 +26,8 @@ def bayesFactors(model1,model2):
   logp1 = model1["deviance"]/(-2.)
   logp2 = model2["deviance"]/(-2.)
 
-  bf = numpy.exp(  pymc.flib.logsum(-logp1)-numpy.log(len(logp1)) 
+  # Given a number of numbers x1, x2, ... whose logarithms are given by l_i=ln(x_i) etc.,
+  # logsum calculates: ln(sum_i exp(l_i)) = ln(sum_i x_i)
+  bf = numpy.exp(  pymc.flib.logsum(-logp1) - numpy.log(len(logp1)) 
                 - (pymc.flib.logsum(-logp2) - numpy.log(len(logp2))) )
   return bf
