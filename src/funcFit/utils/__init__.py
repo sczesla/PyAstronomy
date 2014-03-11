147f22a3e319cde0accbaf75dc3b59abead2e8d0
Stefan <stefan.czesla@hs.uni-hamburg.de>
2014-03-11 17:10:15 +0100

    Bug fix to prevent import error with missing matplotlib.pylab

--- src/funcFit/utils/__init__.py
+++ src/funcFit/utils/__init__.py
@@ -1,9 +1,8 @@
 from PyAstronomy.pyaC import ImportCheck
 
-ic = ImportCheck(["numpy", "scipy", "pymc", "matplotlib", "pyfits"])
+ic = ImportCheck(["numpy", "scipy", "pymc", "matplotlib", "pyfits", "matplotlib.pylab"])
 
-if ic.check["pymc"] and ic.check["numpy"] and ic.check["scipy"] and ic.check["matplotlib"]:
+if ic.check["pymc"] and ic.check["numpy"] and ic.check["scipy"] and ic.check["matplotlib"] and ic.check["matplotlib.pylab"]:
   from anaMCMCTraces import TraceAnalysis
   from bayesFactors import bayesFactors
-
-__all__ = ["TraceAnalysis", "bayesFactors"]
+  __all__ = ["TraceAnalysis", "bayesFactors"]
