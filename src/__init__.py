ecb8e3835461f3697903a8352e0ad2af5ae9b0d9
stefan <stefan.czesla@hs.uni-hamburg.de>
2016-01-28 23:54:11 +0100

    Adding 'six' to requirements for Python 2/3 support

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from PyA_Version import PyA_Version 
+from .PyA_Version import PyA_Version 
 import os
 
 class PyAIS:
@@ -14,13 +14,13 @@ class PyAIS:
       break;
     
   def showStatus(self):
-    print 
-    print "        Results of PyAIS import check:"
-    print "-----------------------------------------------------"
-    print "%20s  |  %5s" % ("Package Name", "Import Status")
-    print "-----------------------------------------------------"
-    for m, v in self.importStatus.iteritems():
-      print "%20s  |  %5s" % (m, v)
+    print() 
+    print("        Results of PyAIS import check:")
+    print("-----------------------------------------------------")
+    print("%20s  |  %5s" % ("Package Name", "Import Status"))
+    print("-----------------------------------------------------")
+    for m, v in self.importStatus.items():
+      print("%20s  |  %5s" % (m, v))
       
   def __init__(self):
     """
@@ -42,6 +42,22 @@ class PyAIS:
 def importCheck():
   p = PyAIS()
   p.showStatus()
+
+
+# Check for the presence of six
+try:
+  import six
+except ImportError:
+  raise(ImportError("""
+  
+  The 'six' package could not be imported.
+  
+  Please install 'six' to use PyAstronomy. The package
+  helps a great deal in providing Python 2/3 (2*3=six) compatible
+  code. It is employed to allow PyA to get ready for Python 3
+  on the long run.  
+  
+  """))
 
 
 # The version Tag for PyAstronomy
