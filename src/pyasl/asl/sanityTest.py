3aaeda70b3710b3887cac68d895a4cd1af5f4b5c
stefan <stefan.czesla@hs.uni-hamburg.de>
2016-04-10 18:53:23 +0200

    Syntax update P23

--- src/pyasl/asl/sanityTest.py
+++ src/pyasl/asl/sanityTest.py
@@ -386,6 +386,58 @@ class SanityOfPyasl(unittest.TestCase, SaniBase):
     print("Coordinates of HD 1 [sexa]: ", sexa)
 
 
+class SanityOfMoonpos(unittest.TestCase, SaniBase):
+  
+  def setUp(self):
+    pass
+  
+  def tearDown(self):
+    pass
+  
+  def sanity_exampleMoonposition(self):
+    """
+      Checking example of moonpos (position)
+    """
+    import datetime
+    from PyAstronomy import pyasl
+    import numpy as np
+    
+    # Convert calendar date to JD
+    # using the datetime package
+    jd = datetime.datetime(2013, 4, 16)
+    jd = pyasl.jdcnv(jd)
+    jd = np.arange(jd, jd + 20, 1)
+    # Calculate Moon positions
+    res = pyasl.moonpos(jd)
+    
+    print("%15s  %8s  %8s  %11s  %8s  %8s" % \
+      ("JD", "RA", "DEC", "DIST", "GEOLON", "GEOLAT"))
+    print("%15s  %8s  %8s  %11s  %8s  %8s" % \
+      ("[d]", "[deg]", "[deg]", "[km]", "[deg]", "[deg]"))
+    for i in range(jd.size):
+      print("%15.4f  %8.4f  %8.4f  %11.4f  %8.4f  %8.4f" % \
+        (jd[i], res[0][i], res[1][i], res[2][i], res[3][i], res[4][i]))
+
+  def sanity_moonphaseExample(self):
+    """
+      Checking exmaple of moonphase
+    """
+    import datetime
+    from PyAstronomy import pyasl
+    import numpy as np
+    
+    # Convert calendar date to JD
+    # using the datetime package
+    jd = datetime.datetime(2013, 4, 16)
+    jd = pyasl.jdcnv(jd)
+    jd = np.arange(jd, jd+20,1)
+    mp = pyasl.moonphase(jd)
+    
+    print("%15s  %3s" % ("JD", "Phase"))
+    for i in range(jd.size):
+      print("%15.4f  %3d%%" % (jd[i], mp[i]*100.))
+
+
 class SanityOfKeplerOrbitExamples(unittest.TestCase, SaniBase):
   
   def setUp(self):
