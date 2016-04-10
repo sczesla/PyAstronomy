3e5e1706683493b2f4b9e82c64ae9bf7902ea837
stefan <stefan.czesla@hs.uni-hamburg.de>
2016-04-10 17:54:25 +0200

    Syntax update P23

--- src/pyasl/asl/sanityTest.py
+++ src/pyasl/asl/sanityTest.py
@@ -1434,31 +1434,31 @@ class SanityOfAltitude(unittest.TestCase, SaniBase):
     alt = 50.
     # Now one wants to know the real altitude of the star, i.e.,
     # the altitude corrected for atmospheric refraction.
-    print
-    print "Get apparent (real) altitude of a star with observed altitude of " + \
-          str(alt) + " degrees"
-    print "  ->  Apparent altitude = ", alt - pyasl.co_refract_forward(alt)
+    print()
+    print("Get apparent (real) altitude of a star with observed altitude of " + \
+          str(alt) + " degrees")
+    print("  ->  Apparent altitude = ", alt - pyasl.co_refract_forward(alt))
     
-    print
-    print "You are not observing from sea level, but from an altitude of 5000 meter."
-    print ("Apparent altitude = %9.5f, estimated pressure [mbar] = %9.5f, " + \
+    print()
+    print("You are not observing from sea level, but from an altitude of 5000 meter.")
+    print(("Apparent altitude = %9.5f, estimated pressure [mbar] = %9.5f, " + \
           "estimated temperature [K] = %9.5f") % \
-          pyasl.co_refract(alt, observer_alt=5000, convert_to_observed=False)
+          pyasl.co_refract(alt, observer_alt=5000, convert_to_observed=False))
     
-    print
-    print "Convert apparent (real) altitude into observed altitude."
-    print "Apparent altitude = " + str(alt) + " degrees",
-    print " -> Observed altitude = " + str(pyasl.co_refract(alt, full_output=False,\
-                                           convert_to_observed=True)[0])
+    print()
+    print("Convert apparent (real) altitude into observed altitude.")
+    print("Apparent altitude = " + str(alt) + " degrees", end=' ')
+    print(" -> Observed altitude = " + str(pyasl.co_refract(alt, full_output=False,\
+                                            convert_to_observed=True)[0]))
     
-    print
-    print "The same object observed from different observer altitudes"
+    print()
+    print("The same object observed from different observer altitudes")
     apparentAltitudes = np.repeat(30.0, 10)
     obsalts = np.linspace(0.,5000.,len(apparentAltitudes))
     r = pyasl.co_refract(apparentAltitudes, observer_alt=obsalts, convert_to_observed=True)
     for i in range(len(r[0])):
-      print "Observed altitude [deg] = %g, pressure [mbar] = %g, temperature [K] = %g" \
-            % (r[0][i], r[1][i], r[2][i])
+      print("Observed altitude [deg] = %g, pressure [mbar] = %g, temperature [K] = %g" \
+            % (r[0][i], r[1][i], r[2][i]))
 
 
 class SanityOfHorizontalCoordinates(unittest.TestCase, SaniBase):
