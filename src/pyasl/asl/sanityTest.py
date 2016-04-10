09e875b89215f61b25ca8ca9957ccbafcb39c1f1
stefan <stefan.czesla@hs.uni-hamburg.de>
2016-04-10 18:55:43 +0200

    Syntax update P23

--- src/pyasl/asl/sanityTest.py
+++ src/pyasl/asl/sanityTest.py
@@ -1399,10 +1399,10 @@ class SanityOfNutation(unittest.TestCase, SaniBase):
     # use the datetime package
     jd = datetime.datetime(2013, 4, 16)
     jd = pyasl.jdcnv(jd)
-    print "Nutation for the date."
+    print("Nutation for the date.")
     res = pyasl.nutate(jd)
-    print "JD = " + str(jd) + ", Longitude = " + str(res[0]) + \
-          ", Obliquity = " + str(res[1])
+    print("JD = " + str(jd) + ", Longitude = " + str(res[0]) + \
+          ", Obliquity = " + str(res[1]))
     
     # Get nutation for an array of JDs.
     startjd = datetime.datetime(2013, 4, 16)
@@ -1410,12 +1410,12 @@ class SanityOfNutation(unittest.TestCase, SaniBase):
     startjd = pyasl.jdcnv(startjd)
     endjd = pyasl.jdcnv(endjd)
     jds = np.arange(startjd, endjd, .5)
-    print
-    print "Plot the results"
-    res = pyasl.nutate(jds, plot=False)
+    print()
+    print("Plot the results")
+    res = pyasl.nutate(jds, plot=True)
     
-    print "Longitude: ", res[0]
-    print "Obliquity: ", res[1]
+    print("Longitude: ", res[0])
+    print("Obliquity: ", res[1])
   
   def sanity_conutateExample(self):
     """
@@ -1432,25 +1432,25 @@ class SanityOfNutation(unittest.TestCase, SaniBase):
     # Specify RA and DEC (degrees)
     ra = 10.
     dec = 30.
-    print "Get change in RA and DEC due to Earth's nutation for JD = " \
-          + str(jd)
-    print pyasl.co_nutate(jd, ra, dec)
+    print("Get change in RA and DEC due to Earth's nutation for JD = " \
+          + str(jd))
+    print(pyasl.co_nutate(jd, ra, dec))
     
-    print
-    print "Get change for several RAs and DECs for the same JD"
+    print()
+    print("Get change for several RAs and DECs for the same JD")
     ra = np.arange(0.,160.,20.)
     dec = np.arange(-80.,80.,20.)
     res = pyasl.co_nutate(np.repeat(jd, ra.size), ra, dec)
-    print res[0], res[1]
+    print(res[0], res[1])
     
-    print
-    print "Get change for several RAs and DECs for different JDs"
+    print()
+    print("Get change for several RAs and DECs for different JDs")
     jds = np.arange(jd,jd+ra.size,1)
     res = pyasl.co_nutate(jds, ra, dec)
-    print "JD             delta(RA)   delta(DEC)"
+    print("JD             delta(RA)   delta(DEC)")
     for i in range(ra.size):
-      print "%12.5f   %8.5f   %8.5f" % (jds[i], res[0][i], res[1][i])
-
+      print("%12.5f   %8.5f   %8.5f" % (jds[i], res[0][i], res[1][i]))
+ 
 
 class SanityOfAberration(unittest.TestCase, SaniBase):
   
