be0caec7c9875a71df08bba664a3172ae1b1ec52
stefan <stefan.czesla@hs.uni-hamburg.de>
2016-04-10 18:05:30 +0200

    Syntax update P23

--- src/pyasl/asl/sanityTest.py
+++ src/pyasl/asl/sanityTest.py
@@ -465,8 +465,8 @@ class SanityOfBinnin(unittest.TestCase, SaniBase):
     r2, dt2 = binningx0dt(x, y, yerr=np.ones(len(x))*0.2, dt=dt1, \
                           x0=-10, useBinCenter=True, removeNoError=True)
     
-    print "dt1, dt2: ", dt1, dt2
-    print "Input data points in last bin: ", r2[-1,3]
+    print("dt1, dt2: ", dt1, dt2)
+    print("Input data points in last bin: ", r2[-1,3])
     
     # Use the reducedBy flag to indicate the binning. In this case, x0
     # will be set to the lowest x value in the data, and the number of
@@ -475,8 +475,8 @@ class SanityOfBinnin(unittest.TestCase, SaniBase):
     r3, dt3 = binningx0dt(x, y, \
                           useBinCenter=True, removeNoError=True, reduceBy=10)
     
-    print "dt3: ", dt3
-    print "Number of bins in theird version: ", len(r3[::,0])
+    print("dt3: ", dt3)
+    print("Number of bins in third version: ", len(r3[::,0]))
     
     
     # Plot the output
@@ -484,7 +484,7 @@ class SanityOfBinnin(unittest.TestCase, SaniBase):
     plt.errorbar(r1[::,0], r1[::,1], yerr=r1[::,2], fmt='kp--')
     plt.errorbar(r2[::,0], r2[::,1], yerr=r2[::,2], fmt='rp--')
     plt.errorbar(r3[::,0], r3[::,1], yerr=r3[::,2], fmt='gp--')
-#    plt.show()
+#     plt.show()
 
   def sanity_example_binningx0dt_example2(self):
     """
@@ -497,9 +497,9 @@ class SanityOfBinnin(unittest.TestCase, SaniBase):
     # Generate some data
     x = np.arange(-100,999)
     # Create some holes in the data
-    x = np.delete(x, range(340,490))
-    x = np.delete(x, range(670,685))
-    x = np.delete(x, range(771,779))
+    x = np.delete(x, list(range(340,490)))
+    x = np.delete(x, list(range(670,685)))
+    x = np.delete(x, list(range(771,779)))
     y = np.sin(x/100.)
     y += np.random.normal(0,0.1,len(x))
     
@@ -512,15 +512,15 @@ class SanityOfBinnin(unittest.TestCase, SaniBase):
     # rebinned time axis.
     r2, dt2 = binningx0dt(x, y, dt=27, x0=min(x), useMeanX=True)
     
-    print "Median shift between the time axes: ", np.median(r1[::,0] - r2[::,0])
-    print " -> Time bins are not aligned due to 'forced' positioning of"
-    print "    the first axis."
+    print("Median shift between the time axes: ", np.median(r1[::,0] - r2[::,0]))
+    print(" -> Time bins are not aligned due to 'forced' positioning of")
+    print("    the first axis.")
     
     # Plot the output
     plt.plot(x,y, 'b.-')
     plt.errorbar(r1[::,0], r1[::,1], yerr=r1[::,2], fmt='kp--')
     plt.errorbar(r2[::,0], r2[::,1], yerr=r2[::,2], fmt='rp--')
-#    plt.show()
+#     plt.show()
 
   def sanity_example_binningx0dt_example3(self):
     """
