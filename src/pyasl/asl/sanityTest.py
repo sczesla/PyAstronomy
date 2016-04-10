832b8d967564ac7da55ce774bda65a3b66ff467a
stefan <stefan.czesla@hs.uni-hamburg.de>
2016-04-10 19:19:30 +0200

    Syntax update P23

--- src/pyasl/asl/sanityTest.py
+++ src/pyasl/asl/sanityTest.py
@@ -1380,10 +1380,10 @@ class SanityOfSunpos(unittest.TestCase, SaniBase):
     # use the datetime package
     jd = datetime.datetime(2013, 4, 16)
     jd = pyasl.jdcnv(jd)
-    print "JD = " + str(jd)
+    print("JD = " + str(jd))
     pos = pyasl.sunpos(jd, full_output=True)
-    print "Coordinates of the Sun (ra, dec): %g, %g" % (pos[1], pos[2])
-    print "Solar elongation = %g and obliquity = %g" % (pos[3], pos[4])
+    print("Coordinates of the Sun (ra, dec): %g, %g" % (pos[1], pos[2]))
+    print("Solar elongation = %g and obliquity = %g" % (pos[3], pos[4]))
     
     # Get the Sun's RA and DEC values for a period of time.
     startjd = datetime.datetime(2013, 4, 16)
@@ -1391,11 +1391,11 @@ class SanityOfSunpos(unittest.TestCase, SaniBase):
     # Convert into Julian dates
     startjd = pyasl.jdcnv(startjd)
     endjd = pyasl.jdcnv(endjd)
-    print
+    print()
     pos = pyasl.sunpos(startjd, end_jd=endjd, jd_steps=10, plot=False, full_output=True)
     
     for i in range(len(pos[0])):
-      print "At JD = %g: ra = %g, dec = %g" % (pos[0][i], pos[1][i], pos[2][i])
+      print("At JD = %g: ra = %g, dec = %g" % (pos[0][i], pos[1][i], pos[2][i]))
 
 
 class SanityOfNutation(unittest.TestCase, SaniBase):
