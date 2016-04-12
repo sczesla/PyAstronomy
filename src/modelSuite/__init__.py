e655d8979af252cb9e9d62d06cfad39f84915da1
stefan <stefan.czesla@hs.uni-hamburg.de>
2016-04-12 22:50:53 +0200

    Syntax update P23

--- src/modelSuite/__init__.py
+++ src/modelSuite/__init__.py
@@ -2,14 +2,14 @@ from PyAstronomy.pyaC import ImportCheck
 
 ic = ImportCheck(["numpy", "scipy"], required=["numpy"])
 
-from XTran import palTrans
-from XTran import forTrans
-from XTran import LimBrightTrans, RmcL, RmcL_Hirano
-from radVel import SinRadVel
-from planetBrightnessPhases import GeomPlanetBrightPhase
-from keplerEllipseModel import KeplerEllipseModel
+from .XTran import palTrans
+from .XTran import forTrans
+from .XTran import LimBrightTrans, RmcL, RmcL_Hirano
+from .radVel import SinRadVel
+from .planetBrightnessPhases import GeomPlanetBrightPhase
+from .keplerEllipseModel import KeplerEllipseModel
 #from atanProfile import AtanProfile, AtanProfileDamped
-from lineListGaussModel import *
-from voigtAstro import VoigtAstroP
-from LyAProfile import LyaTransmission
-from rotBroadProfile import RotBroadProfile
+from .lineListGaussModel import *
+from .voigtAstro import VoigtAstroP
+from .LyAProfile import LyaTransmission
+from .rotBroadProfile import RotBroadProfile
