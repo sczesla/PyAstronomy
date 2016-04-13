e684afb5fd9cf83dcd81b7f1df4b6859bdb4a9f7
stefan <stefan.czesla@hs.uni-hamburg.de>
2016-04-13 21:18:59 +0200

    Syntax update P23

--- src/pyaGui/__init__.py
+++ src/pyaGui/__init__.py
@@ -2,7 +2,7 @@ from PyAstronomy.pyaC import ImportCheck
 
 ic = ImportCheck(["Tkinter", "matplotlib"], required=["Tkinter", "matplotlib"])
 
-from pyaPicker import Picker
-from ffmodelExplorer import FFModelExplorer, FFModelExplorerList, FFModelPlotFit, ffmodelExplorer
-from continuumFinder import ContinuumInteractive
-from interactiveGV import IAGVFit
+from .pyaPicker import Picker
+from .ffmodelExplorer import FFModelExplorer, FFModelExplorerList, FFModelPlotFit, ffmodelExplorer
+from .continuumFinder import ContinuumInteractive
+from .interactiveGV import IAGVFit
