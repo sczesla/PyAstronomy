acaf37c957d33c3a1f37768e60d42e548cd5b7a1
Stefan Czesla <stefan.czesla@hs.uni-hamburg.de>
2016-08-04 23:49:52 +0200

    Docu, p2/3 compatibility, left/right -> response/pmodel

--- src/funcFit/respModel.py
+++ src/funcFit/respModel.py
@@ -1,17 +1,37 @@
-#import PyAstronomy.funcFit as fuf
-import matplotlib.pyplot as plt
-import numpy as np
+from __future__ import print_function, division
 from PyAstronomy.pyaC import pyaErrors as PE
 import copy
 from .onedfit import OneDFit
+import six
 
 class ConvolutionModel(OneDFit):
     """
-      Provides functionality to convolve a funcFit with a response
-      while retaining the full fitting capabilities of funcFit models.
+      Generate a convolved funcFit model with full fitting functionality.
       
-      Example:
-      --------
+      In many cases, it can be useful to modify a model by a convolution with
+      some function. Examples if this comprise a spectral response function or
+      instrumental broadening in optical high-resolution spectra.  
+      
+      This model 'wraps around' the original model, uses its output, and convolves
+      it with some kernel. 
+      
+      Parameters
+      ----------
+      pmodel - OneDFit instance
+          FuncFit model representing the model to be folded with the response.
+      resp : class instance
+          A class instance, which must provide a 'convolve' method, which
+          must be of the form convolve(x, y) and the result must satisfy
+          the condition len(convolve(x, y)) == len(x). The convolve method
+          is called whenever the model is evaluated. In particular, the first
+          parameter is the x-values it which to evaluate and the second parameter
+          is the result of the evaluation of 'pmodel'. If the class instance is
+          also of type 'OneDFit', i.e., a funcFit object, it can have free
+          parameters adapted in the fitting process.
+        
+      
+      Example
+      -------
       import PyAstronomy.funcFit as fuf
       import matplotlib.pyplot as plt
       import numpy as np
@@ -49,43 +69,53 @@ class ConvolutionModel(OneDFit):
       
     """
     def __init__(self, resp, pmodel):
-        """
-        
-          Parameters
-          ----------
-          resp - class instance; must provide the method 'convolve', which
-                 must be of the form convolve(x, y) with len(convolve(x, y))==len(x).
-                 Represents the instrument response.
-                 Class can be derived from OneDFit so that its parameters
-                 can be varied during the fit simultaneously with the 
-                 parameters of the physical model
-          pmodel - OneDFit instance
-                   Physical model
-        """
         
         # check if response model has convolve method
         if not hasattr(resp, "convolve"):
-            raise(PE.pyaErrors.pyaOtherErrors.PyAAlgorithmFailure("Response model has no method 'convolve'.", \
+            raise(PE.PyAAlgorithmFailure("Response model has no method 'convolve'.", \
                               where="ConvolutionModel::__init__"))
+            
         if not isinstance(pmodel, OneDFit):
-            raise(PE.pyaErrors.pyaOtherErrors.PyAAlgorithmFailure("Physical model must be derived from OneDFit", \
+            raise(PE.PyAAlgorithmFailure("Physical model must be derived from OneDFit", \
                               where="ConvolutionModel::__init__"))
-        # Sollte man hier ein copy.deepcopy verwenden?
-        self.left = copy.deepcopy(resp)
-        self.right = copy.deepcopy(pmodel)
-        if isinstance(self.left, OneDFit):
-            self.left.setRootName("Response")
-            self._combineRemapping(self.left, self.right)
-            parameters = self.left.parameters().keys() + self.right.parameters().keys()
+            
+        # Copy the input models
+        self.response = copy.deepcopy(resp)
+        self.pmodel = copy.deepcopy(pmodel)
+        
+        if isinstance(self.response, OneDFit):
+            # The response is also a funcFit model and, therefore, could
+            # already contribute fitting parameters
+            # 
+            # Change root name to "Response"
+            self.response.setRootName("Response")
+            # Ensure unique parameter names    
+            self._combineRemapping(self.response, self.pmodel)
+            # List of parameters for the this (the convolved) model
+            parameters = list(self.response.parameters()) + list(self.pmodel.parameters())
         else:
-            parameters = self.right.parameters().keys()
+            # List of parameters for the this (the convolved) model
+            parameters = list(self.pmodel.parameters())
+        
+        # Set up funcFit model and assign
         OneDFit.__init__(self, parameters)
-        self.convolve = self.left.convolve
-        self.setRootName("ConvolutionModel")
+        
+        # Define root name
+        if self.pmodel.naming._root == "":
+          # Use default 
+          rn = "model"
+        else:
+          rn = self.pmodel.naming._root
+        
+        self.setRootName("Conv-" + rn)
+        
     def evaluate(self, x):
-        if isinstance(self.left, OneDFit):
-          for p in self.left.parameters().keys():
-            self.left[p] = self[p]
-        for p in self.right.parameters().keys():
-          self.right[p] = self[p]          
-        return self.convolve(x, self.right.evaluate(x))
+        
+        if isinstance(self.response, OneDFit):
+            # Assign parameters if OneDFit instance
+            for p in six.iterkeys(self.response.parameters()):
+                self.response[p] = self[p]
+        for p in six.iterkeys(self.pmodel.parameters()):
+          self.pmodel[p] = self[p]
+               
+        return self.response.convolve(x, self.pmodel.evaluate(x))
