c9be32f6231d02f3e9525f227827fac7983e2964
Stefan Czesla <stefan.czesla@hs.uni-hamburg.de>
2016-08-08 13:12:24 +0200

    Added VO table validation in case of read failure

--- src/pyasl/resBased/exoplanetEU.py
+++ src/pyasl/resBased/exoplanetEU.py
@@ -346,7 +346,11 @@ class ExoplanetEU2(pp.PyAUpdateCycle):
     # Temporarily suppress warnings (astropy issues one on reading this table)
     with warnings.catch_warnings():
       warnings.simplefilter("ignore")
-      self.vot = votable.parse(self._fs.requestFile(self.dataFileName, 'r', gzip.open))
+      try:
+        self.vot = votable.parse(self._fs.requestFile(self.dataFileName, 'r', gzip.open), pedantic=False)
+      except Exception as e:
+        votable.validate(self._fs.requestFile(self.dataFileName, 'r', gzip.open))
+        raise e
     # Use 'name' over ID field to specify column names
     self.vot = self.vot.get_first_table().to_table(use_names_over_ids=True)
   
