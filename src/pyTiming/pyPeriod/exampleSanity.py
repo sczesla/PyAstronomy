3b4dd8125721ed30343f04fe1e7b6114d06074ef
Stefan <stefan.czesla@hs.uni-hamburg.de>
2017-04-18 10:47:59 +0200

    Checking example sanity for GLS

--- src/pyTiming/pyPeriod/exampleSanity.py
+++ src/pyTiming/pyPeriod/exampleSanity.py
@@ -94,3 +94,146 @@ class PyPeriodExSanity(unittest.TestCase):
     print("GLS maximum power: ", maxPower)
     print("GLS statistics of maximum power peak: ", gls.stats(maxPower))
 #    gls.plot(block=True)
+
+
+
+class PyPeriodGLSSanity(unittest.TestCase):
+  
+  def setUp(self):
+    pass
+  
+  def tearDown(self):
+    pass
+  
+  def sanity_example1(self):
+    """
+    Checking: Simple periodogram with default options
+    """
+#     from __future__ import print_function, division
+    import numpy as np
+    import matplotlib.pylab as plt
+    from PyAstronomy.pyTiming import pyPeriod
+    
+    # Create some evenly sampled data including a periodic term.
+    # Number of data points, input frequency, amplitude of input signal,
+    # and standard deviation of noise.
+    N = 1000
+    f = 0.1
+    A = 0.15
+    sig = 0.2
+    
+    time = np.arange(float(N))
+    flux = A * np.sin(2.* np.pi*time*f)
+    # Adding the noise
+    flux += np.random.normal(0, sig, time.size)
+    
+    # Compute the GLS periodogram with default options.
+    clp = pyPeriod.Gls((time, flux))
+    
+    # Print helpful information to screen
+    clp.info()
+    
+    # and plot power vs. frequency.
+#     plt.xlabel("Frequency")
+#     plt.ylabel("Power")
+#     plt.plot(clp.freq, clp.power, 'b.-')
+#     plt.show()
+    
+  def sanity_example2(self):
+    """
+    Checking: Adding an error column and FAP levels
+    """
+#     from __future__ import print_function, division
+    import numpy as np
+    import matplotlib.pylab as plt
+    from PyAstronomy.pyTiming import pyPeriod
+    
+    # Create some evenly sampled data including a periodic term.
+    # Number of data points, input frequency, amplitude of input signal,
+    # and standard deviation of noise.
+    N = 1000
+    f = 0.1
+    A = 0.05
+    sig = 0.2
+    
+    time = np.arange(float(N))
+    flux = A * np.sin(2.* np.pi*time*f)
+    # Adding the noise
+    flux += np.random.normal(0, sig, time.size)
+    # Adding an error column
+    err = np.ones(N)*sig
+    
+    # Compute the GLS periodogram with default options.
+    # Choose Zechmeister-Kuerster normalization explicitly
+    clp = pyPeriod.Gls((time, flux, err), norm="ZK")
+    
+    # Print helpful information to screen
+    clp.info()
+    
+    # Define FAP levels of 10%, 5%, and 1%
+    fapLevels = np.array([0.1, 0.05, 0.01])
+    # Obtain the associated power thresholds
+    plevels = clp.powerLevel(fapLevels)
+    
+#     # and plot power vs. frequency.
+#     plt.xlabel("Frequency")
+#     plt.ylabel("Power")
+#     plt.plot(clp.freq, clp.power, 'b.-')
+#     # Add the FAP levels to the plot
+#     for i in range(len(fapLevels)):
+#         plt.plot([min(clp.freq), max(clp.freq)], [plevels[i]]*2, '--',
+#         label="FAP = %4.2f%%" % fapLevels[i])
+#     plt.legend()
+#     
+#     plt.show()
+
+  def sanity_example3(self):
+    """
+    Checking: Getting highest-power (best-fit) sine curve
+    """
+#     from __future__ import print_function, division
+    import numpy as np
+    import matplotlib.pylab as plt
+    from PyAstronomy.pyTiming import pyPeriod
+    
+    # Create some evenly sampled data including a periodic term.
+    # Number of data points, input frequency, amplitude of input signal,
+    # and standard deviation of noise.
+    N = 1000
+    f = 0.171
+    A = 0.5
+    sig = 0.2
+    
+    time = np.arange(float(N))
+    flux = A * np.sin(2.* np.pi*time*f)
+    # Adding the noise
+    flux += np.random.normal(0, sig, time.size)
+    # Adding an error column
+    err = np.ones(N)*sig
+    
+    # Compute the GLS periodogram with default options.
+    # Choose Zechmeister-Kuerster normalization explicitly
+    clp = pyPeriod.Gls((time, flux, err), norm="ZK")
+    
+    # Get index associated with highest power
+    ifmax = np.argmax(clp.power)
+    # and highest power and associated frequency
+    pmax = clp.power[ifmax]
+    fmax = clp.freq[ifmax]
+    # Convert frequency into period
+    hpp = 1./fmax
+    print("Highest-power period: ", hpp)
+    
+    # Calculate sine wave associated with 'best-fit' frequency
+    bestSine = clp.sinmod(time)
+    
+#     plt.subplot(2,1,1)
+#     plt.title("Data and sine asscoiated with highest-power frequency")
+#     plt.plot(time, flux, 'b.')
+#     plt.plot(time, bestSine, 'r--')
+#     plt.subplot(2,1,2)
+#     plt.title("Folded data")
+#     plt.plot(time/hpp-np.floor(time/hpp), flux, 'b.')
+#     plt.show()
+
+
