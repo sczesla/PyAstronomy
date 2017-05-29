191431c1b95cfb38ede8b13fd914e90d8164c624
Christian <astro@pcschneider.eu>
2017-05-29 17:04:31 +0200

    Corrected import of poissonPosterior

--- src/pyasl/asl/aslExt_1/__init__.py
+++ src/pyasl/asl/aslExt_1/__init__.py
@@ -4,6 +4,7 @@ from PyAstronomy.pyasl import _ic, _moduleImportStatus
 _modules = ["pizzolato2003", "expCorrRN", "ramirez2005", "stellarAge", "ballesterosBV_T", \
            "abundances", "spectralClass", "mmscetsTable", "poissonPosterior"]
 
+
 for m in _modules:
   _moduleImportStatus[(__name__, m)] = pyaC.pyaimportallfrom(m, "PyAstronomy.pyasl.asl.aslExt_1", globals())
 
