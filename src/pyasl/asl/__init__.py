fc45d6337b3884a5e22395ade56e0b27259ab285
Stefan <stefan.czesla@hs.uni-hamburg.de>
2017-10-30 16:58:41 +0100

    Import betaSig

--- src/pyasl/asl/__init__.py
+++ src/pyasl/asl/__init__.py
@@ -5,7 +5,7 @@ from PyAstronomy.pyasl import _moduleImportStatus
 _modules = ["astroTimeLegacy", "airtovac", "aitoffLegacy", "folding", \
            "keplerOrbit", "inTransit", "statTest", "binning", "smooth", \
            "planet_Teq", "planck", "planetPhase", "fluxConversion", "lineOfSight", \
-           "lineOfSight", "dopplerShift", "fluxConversion", "baryvel"]
+           "lineOfSight", "dopplerShift", "fluxConversion", "baryvel", "betaSig"]
 
 
 _modules.extend(["idlMod", "idlCirrange", "rotBroad", "svd", "unred", "estimateSNR", \
@@ -21,5 +21,5 @@ _modules.extend(["moonpos", "moonphase", "localtime", "lineWidth", "cardinalPoin
 from .aslExt_1 import *
 
 for m in _modules:
-  _moduleImportStatus[(__name__, m)] = pyaC.pyaimportallfrom(m, "PyAstronomy.pyasl.asl", globals())
+    _moduleImportStatus[(__name__, m)] = pyaC.pyaimportallfrom(m, "PyAstronomy.pyasl.asl", globals())
  
