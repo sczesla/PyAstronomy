b87d3fedcce4f1da521440744c5893d52057d427
Stefan <stefan.czesla@hs.uni-hamburg.de>
2018-01-31 15:59:18 +0100

    Added numpy to the list of required packages

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ setup(cmdclass={"with-ext": WithExtCommand},
       packages=packages,
       ext_modules=ext_modules,
       package_dir={'PyAstronomy': 'src'},
-      install_requires=['six'],
+      install_requires=['numpy', 'six'],
       # Do not forget to give the ``correct'' name for the module! (here, e.g., PyAstronomy.funcFit)
       package_data=package_data,
       author='PyA group',
