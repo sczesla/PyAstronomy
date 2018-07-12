740274515e10576022a45fa9a024c5ca31289feb
Stefan <stefan.czesla@hs.uni-hamburg.de>
2018-07-12 16:28:28 +0200

    PEP8 for mandelAgol

--- src/modelSuite/XTran/forTrans/mandelAgol.py
+++ src/modelSuite/XTran/forTrans/mandelAgol.py
@@ -5,43 +5,43 @@ from PyAstronomy.pyaC import pyaErrors as PE
 from PyAstronomy.modelSuite.XTran import _ZList
 
 try:
-  from . import occultquad
-  _importOccultquad = True
+    from . import occultquad
+    _importOccultquad = True
 except ImportError:
-  _importOccultquad = False
+    _importOccultquad = False
 
 try:
-  from . import occultnl
-  _importOccultnl = True
+    from . import occultnl
+    _importOccultnl = True
 except ImportError:
-  _importOccultnl = False
+    _importOccultnl = False
 
-    
+
 class MandelAgolLC(_ZList, fuf.OneDFit):
-  """
+    """
     Analytical transit light-curves using the formulae provided by Mandel & Agol 2002.
-    
+
     .. note :: The computation of transit light curves
                is done using the external *occultquad* FORTRAN library.
                This library must be compiled manually using SciPy's f2py
                wrapper (http://www.scipy.org/F2py). Simply go to the
                *forTrans* directory of the source distribution of PyAstronomy,
                then invoke
-               
+
                f2py -c occultquad.pyf occultquad.f
-               
+
                f2py -c occultnl.pyf occultnl.f
-               
+
                That's it. 
-    
+
     :Model parameters:
-    
+
     The set of parameters specifying this model depends on: the type
     of orbit chosen (circular or keplerian) and the type of limb darkening
     chosen (quadratic or non-linear).
-    
+
     More information on the Keplerian orbit can be found here: :ref:`keplerorbitpyasl`
-    
+
     *Orbital model parameters (circular orbit)*:
       - `p` - Radius ratio between planet and star.
       - `a` - Semi-major axis of planetary orbit [stellar radii].
@@ -64,7 +64,7 @@ class MandelAgolLC(_ZList, fuf.OneDFit):
     *Limb darkening parameters (quadratic)*:
       - `linLib` - Linear limb-darkening coefficient.
       - `quadLimb` - Quadratic limb-darkening coefficient.
-    
+
     *Limb darkening parameters (non-linear)*:
       - `a1` - Non-Linear limb-darkening coefficient.
       - `a2` - Non-Linear limb-darkening coefficient.
@@ -72,18 +72,18 @@ class MandelAgolLC(_ZList, fuf.OneDFit):
       - `a4` - Non-Linear limb-darkening coefficient.
 
     :Limb-darkening laws:
-    
+
     The quadratic limb-darkening law is given by:
-    
+
     .. math :: \\frac{I(\mu)}{I(1)}= 1 - linLimb \\times (1-\mu) - quadLimb \\times (1-\mu)^2
-    
+
     The non-linear limb-darkening law is given by:
-    
+
     .. math :: \\frac{I(\mu)}{I(1)}= 1 - \\sum_{n=1}^{4}{a_n(1-\mu^{n/2})}
 
     .. warning::
         Time units have to be consistent.
-        
+
     Parameters
     ----------
     orbit : string, {"circular", "keplerian"}, optional
@@ -99,91 +99,93 @@ class MandelAgolLC(_ZList, fuf.OneDFit):
         is a physical collision between the star and the planet
         on evaluating the model and raises an exception when there
         is one.
-  """
-  
-  def __init__(self, orbit="circular", ld="quad", collCheck=True):
-    if not orbit in ["circular", "keplerian"]:
-      raise(PE.PyAValError("Invalid option for orbit: " + str(orbit), \
-                           soltuion="Use either 'circular' or 'keplerian'."))
-    if not ld in ["quad", "nl"]:
-      raise(PE.PyAValError("Invalid option for orbit: " + str(ld), \
-                           soltuion="Use either 'quad' or 'nl'."))      
-    _ZList.__init__(self, orbit, collCheck)
-    
-    if (not _importOccultquad) and (ld == "quad"):
-        raise(PE.PyARequiredImport("Could not import required shared object library 'occultquad.so'",\
-                                   solution=["Invoke PyA's install script (setup.py) with the --with-ext option.",
-                                             "Go to 'forTrans' directory of PyAstronomy and invoke\n    f2py -c occultquad.pyf occultquad.f"]
-                                   ))
-    
-    if (not _importOccultnl) and (ld == "nl"):
-        raise(PE.PyARequiredImport("Could not import required shared object library 'occultquad.so'",\
-                                   solution=["Invoke PyA's install script (setup.py) with the --with-ext option.",
-                                             "Go to 'forTrans' directory of PyAstronomy and invoke\n    f2py -c occultnl.pyf occultnl.f"]
-                                   ))
-    
-    if orbit == "circular":
-      plist = ["p", "a", "i", "T0", "per", "b"]
-    else:
-      # It is an elliptical orbit
-      plist = ["p", "a", "i", "per", "b", \
-               "e", "Omega", "tau", "w"]
-    
-    if ld == "quad":
-      plist.extend(["linLimb", "quadLimb"])
-    else:
-      # Non-linear LD
-      plist.extend(["a1", "a2", "a3", "a4"])
-    
-    fuf.OneDFit.__init__(self, plist)
-    self.freeze(plist)
-    self.setRootName("Occultquad")
-    
-    if orbit == "keplerian":
-      self["w"] = -90.0
-      
-    self._orbit = orbit
-    self._ld = ld
-    
-  def evaluate(self, time):
-    """ 
-     Calculate a light curve according to the analytical models
-     given by Mandel and Agol.
-        
-     Parameters
-     ----------
-     time : array
-         An array of time points at which the light curve
-         shall be calculated.
-
-     Returns
-     -------
-     Model : array
-         The analytical light curve is stored in the property `lightcurve`.
     """
 
-    # Translate the given parameters into an orbit and, finally,
-    # into a projected, normalized distance (z-parameter)
-    if self._orbit == "circular":
-      self._calcZList(time - self["T0"])
-    else:
-      # The orbit is keplerian
-      self._calcZList(time)
+    def __init__(self, orbit="circular", ld="quad", collCheck=True):
+        if not orbit in ["circular", "keplerian"]:
+            raise(PE.PyAValError("Invalid option for orbit: " + str(orbit),
+                                 soltuion="Use either 'circular' or 'keplerian'."))
+        if not ld in ["quad", "nl"]:
+            raise(PE.PyAValError("Invalid option for orbit: " + str(ld),
+                                 soltuion="Use either 'quad' or 'nl'."))
+        _ZList.__init__(self, orbit, collCheck)
 
-    # Use occultquad Fortran library to compute flux decrease
-    df = numpy.zeros(len(time))
-    if len(self._intrans) > 0:
-      if self._ld == "quad":
+        if (not _importOccultquad) and (ld == "quad"):
+            raise(PE.PyARequiredImport("Could not import required shared object library 'occultquad.so'",
+                                       solution=["Invoke PyA's install script (setup.py) with the --with-ext option.",
+                                                 "Go to 'forTrans' directory of PyAstronomy and invoke\n    f2py -c occultquad.pyf occultquad.f"]
+                                       ))
+
+        if (not _importOccultnl) and (ld == "nl"):
+            raise(PE.PyARequiredImport("Could not import required shared object library 'occultquad.so'",
+                                       solution=["Invoke PyA's install script (setup.py) with the --with-ext option.",
+                                                 "Go to 'forTrans' directory of PyAstronomy and invoke\n    f2py -c occultnl.pyf occultnl.f"]
+                                       ))
+
+        if orbit == "circular":
+            plist = ["p", "a", "i", "T0", "per", "b"]
+        else:
+            # It is an elliptical orbit
+            plist = ["p", "a", "i", "per", "b",
+                     "e", "Omega", "tau", "w"]
+
+        if ld == "quad":
+            plist.extend(["linLimb", "quadLimb"])
+        else:
+            # Non-linear LD
+            plist.extend(["a1", "a2", "a3", "a4"])
+
+        fuf.OneDFit.__init__(self, plist)
+        self.freeze(plist)
+        self.setRootName("Occultquad")
+
+        if orbit == "keplerian":
+            self["w"] = -90.0
+
+        self._orbit = orbit
+        self._ld = ld
+
+    def evaluate(self, time):
+        """ 
+        Calculate a light curve according to the analytical models
+        given by Mandel and Agol.
+
+        Parameters
+        ----------
+        time : array
+            An array of time points at which the light curve
+            shall be calculated.
+
+        Returns
+        -------
+        Model : array
+            The analytical light curve is stored in the property `lightcurve`.
+        """
+
+        # Translate the given parameters into an orbit and, finally,
+        # into a projected, normalized distance (z-parameter)
+        if self._orbit == "circular":
+            self._calcZList(time - self["T0"])
+        else:
+            # The orbit is keplerian
+            self._calcZList(time)
+
         # Use occultquad Fortran library to compute flux decrease
-        result = occultquad.occultquad(self._zlist[self._intrans],self["linLimb"],self["quadLimb"], \
-                                       self["p"],len(self._intrans))
-      else:
-        result = occultnl.occultnl(self["p"],self["a1"],self["a2"],self["a3"], \
-                                   self["a4"],self._zlist[self._intrans])
-      df[self._intrans] = (1.0 - result[0])
-      
-    self.lightcurve = (1.-df)*1./(1.+self["b"]) + self["b"]/(1.0+self["b"])
-    
-    return self.lightcurve
+        df = numpy.zeros(len(time))
+        if len(self._intrans) > 0:
+            if self._ld == "quad":
+                # Use occultquad Fortran library to compute flux decrease
+                result = occultquad.occultquad(self._zlist[self._intrans], self["linLimb"], self["quadLimb"],
+                                               self["p"], len(self._intrans))
+            else:
+                result = occultnl.occultnl(self["p"], self["a1"], self["a2"], self["a3"],
+                                           self["a4"], self._zlist[self._intrans])
+            df[self._intrans] = (1.0 - result[0])
+
+        self.lightcurve = (1. - df) * 1. / \
+            (1. + self["b"]) + self["b"] / (1.0 + self["b"])
+
+        return self.lightcurve
+
 
 MandelAgolLC_Rebin = fuf.turnIntoRebin(MandelAgolLC)
