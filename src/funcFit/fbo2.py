89bd4e2c6c463c62ee58a674eb580c316f86c015
Stefan Czesla <stefan.czesla@hs.uni-hamburg.de>
2018-07-24 13:06:42 +0200

    stat

--- src/funcFit/fbo2.py
+++ src/funcFit/fbo2.py
@@ -11,6 +11,7 @@ from .extFitter import NelderMead
 import six
 import six.moves as smo
 import bidict
+import collections
 
 from PyAstronomy.funcFit import _pymcImport, _scoImport, ic
 
@@ -38,7 +39,6 @@ class PyAPa(object):
         self._value = value
         self.free = False
         self.affects = set()
-        self.affectsPriors = set()
         self.relation = None
         self.dependsOn = None
         self.freeable = True
@@ -71,7 +71,7 @@ class PyARelation(object):
     
 class PyAPrior(object):
     
-    def __init__(self, ivs, func):
+    def __init__(self, ivs, func, descr=None):
         """
         Parameters
         ----------
@@ -80,12 +80,23 @@ class PyAPrior(object):
         func : Callable
             Function returning log(prior) depending on the value of the
             independent parameters
+        descr : string, optional
+            Description of the prior
         """
+        if not hasattr(ivs, "__iter__"):
+            ivs = [ivs]
         self.ivs = ivs
         self.func = func
+        if descr is None:
+            self.descr = "Pya Prior"
+        else:
+            self.descr = descr
     
-    def update(self):
+    def evaluate(self):
         return self.func(*[p.value for p in self.ivs])
+    
+    def __str__(self):
+        return self.descr
     
       
 class PyABPS(object):
@@ -160,8 +171,7 @@ class PyABaSoS(object):
         return set([b.number if b.rootName == rn else 0 for b in self.bss])
     
     def _updatepmap(self):
-        """
-        """
+        """ Update parameter names according to root name and number. """
         ar = self._allRoots()
         if len(ar) == 1:
             # Only one subset. Apply no name updating
@@ -203,13 +213,15 @@ class PyABaSoS(object):
             raise(PE.PyAValError("No such parameter: " + str(n),
                                  solution="Use one of: " + ', '.join(list(self.pmap))))
     
-    def __getitem__(self, n, ref=False):
+    def __getitem__(self, n):
+        """ Get value of parameter """
+        self._checkParam(n)
+        return self.pmap[n].value
+    
+    def getPRef(self, n):
         """ Get reference to PyaPa """
         self._checkParam(n)
-        if ref:
-            return self.pmap[n]
-        else:
-            return self.pmap[n].value
+        return self.pmap[n]
     
     def __setitem__(self, n, v):
         """ Set value and update related if necessary """
@@ -318,10 +330,81 @@ class PyABaSoS(object):
             return None, None, None
         else:
             return r.group(0), r.group(3), r.group(5)
+   
+    def parameters(self):
+        """
+        Get all parameter names and values
+        
+        Returns
+        -------
+        Parameters : OrderedDict (collections)
+            (Ordered) Dictionary mapping parameter name to value
+        """
+        ps = collections.OrderedDict()
+        for k, v in six.iteritems(self.pmap):
+            ps[k] = v.value
+        return ps
     
-    def addPrior(self, pyap):
-        for p in pyap.ivs:
-            p.affectsPriors.update(pyap)
+    def freeParamNames(self):
+        """ Get list of names of free parameters """
+        return [p for p in list(self.pmap) if self.pmap[p].free]
+    
+    def freeParamVals(self):
+        """ Get list of names of free parameters """
+        return [self.pmap[p].value for p in list(self.pmap) if self.pmap[p].free]
+    
+    def setFreeParamVals(self, vals):
+        """
+        Assign values to free parameters.
+        
+        The order is fixed (use, e.g., freeParamNames to get it).
+        
+        Parameters
+        ----------
+        vals : list or array
+            The values to be assigned
+        """
+        for i, p in enumerate(self.freeParamNames()):
+            self[p] = vals[i]
+    
+    def _thawfreeze(self, pns, free):
+        """
+        Thaw or freeze parameters
+        
+        Parameters
+        ----------
+        pns : string or list of strings
+            Relevant parameters
+        free : boolean
+            True to thaw and False to freeze
+        """
+        if isinstance(pns, six.string_types):
+            pns = [pns]
+        for p in pns:
+            self._checkParam(p)
+            self.pmap[p].free = True
+    
+    def thaw(self, pns):
+        """
+        Thaw parameters
+        
+        Parameters
+        ----------
+        pns : string or list of strings
+            Relevant parameters
+        """
+        self._thawfreeze(pns, True)
+    
+    def freeze(self, pns):
+        """
+        Freeze parameters
+        
+        Parameters
+        ----------
+        pns : string or list of strings
+            Relevant parameters
+        """
+        self._thawfreeze(pns, False)
     
     def __init__(self, *args):
         self.bss = []
@@ -329,8 +412,6 @@ class PyABaSoS(object):
             self.addBPS(s)
         self._updatepmap()
       
-      
-    
 
 class MBO2(object):
     """
@@ -411,31 +492,27 @@ class MBO2(object):
     def evaluate(self, *args, **kwargs):
         pass
     
-    def logL(self, *args, **kwargs):
-        pass
-    
     def relate(self, dv, idv, func=None):
         self.pars.relate(dv, idv, func)
-    
-    def _defLogL(self, *args, **kwargs):
-        if len(args) == 2:
-            x, y = args[0], args[1]
-            m = self.evaluate(x)
-            return np.sum((m - y)**2)
-        elif len(args) == 3:
-            x, y, yerr = args[0], args[1], args[2]
-            m = self.evaluate(x)
-            return np.sum((m-y)**2/yerr**2)
-        else:
-            raise(PE.PyAValError("Invalid call to _defLogL. No. of arguments: " + str(len(args))))
     
     def __getitem__(self, n):
         return self.pars[n]
     
     def __setitem__(self, n, v):
         self.pars[n] = v
-        
     
+    def getPRef(self, n):
+        return self.pars.getPRef(n)
+    
+    def freeParamVals(self):
+        return self.pars.freeParamVals()
+    
+    def freeParamNames(self):
+        return self.pars.freeParamNames()
+    
+    def setFreeParamVals(self, vals):
+        self.pars.setFreeParamVals(vals)
+        
     
 class Poly2(MBO2):
     
@@ -452,4 +529,92 @@ class Poly2(MBO2):
     
 
 
+class PStat(object):
+    
+    def setMode(self, mode):
+        if mode == "default":
+            self.logL = self._deflogL
+        elif (mode is None) or (mode == "manual"):
+            pass
+        else:
+            raise(PE.PyAValError("Unknown mode for PyABay: " + str(mode)))
+    
+    def __init__(self, model, statmode="default"):
+        self.priors = []
+        self.setMode(statmode)
+        self.statmode = statmode
+        self._model = model
+        
+    def model(self):
+        return self._model
+    
+    def logL(self, *args, **kwargs):
+        raise(PE.PyANotImplemented("The method 'logL' needs to be implemented."))
+
+    def logPrior(self, *args, **kwargs):
+        result = 0.0
+        for p in self.priors:
+            result += p.evaluate()
+            if np.isinf(result):
+                return result
+        return result
+    
+    def addUniformPrior(self, pn, lower=None, upper=None):
+        """
+        """
+        if (lower is None) and (upper is None):
+            f = lambda x:0.0
+        elif (not lower is None) and (upper is None):
+            f = lambda x:0.0 if x < upper else -np.inf
+        elif (lower is None) and (not upper is None):
+            f = lambda x:0.0 if x > lower else -np.inf
+        else:
+            if upper <= lower:
+                raise(PE.PyAValError("'lower' must be smaller than 'upper'", \
+                                     where="addUniformPrior"))
+            r = upper - lower
+            f = lambda x:np.log(r) if (x >= lower) and(x <=upper) else -np.inf
+        
+        self._model.pars._checkParam(pn)
+        descr = "Uniform prior on '" + str(pn) + "' (lower = %g, upper = %g)" % (lower, upper)
+        self.priors.append(PyAPrior(self._model.getPRef(pn), f, descr=descr))
+        
+    def addPrior(self, p):
+        pass
+    
+    def margD(self):
+        return None
+
+    def _deflogL(self, *args, **kwargs):
+        
+        if len(args) == 2:
+            x, y = args[0], args[1]
+            yerr = 1.0
+        elif len(args) == 3:
+            x, y, yerr = args[0], args[1], args[2]
+        else:
+            raise(PE.PyAValError("Invalid call to logL of PyABayDef. No. of arguments: " + str(len(args))))
+
+        if "_currentModel" in kwargs:
+            m = kwargs["_currentModel"]
+        else:
+            m = self._model.evaluate(x)
+        
+        return -np.sum((m-y)**2/yerr**2)
+
+    def logPost(self, *args, **kwargs):
+        """ Get log of the posterior """
+        lp = self.logPrior(*args, **kwargs) + self.logL(*args, **kwargs)
+        md = self.margD()
+        if not md is None:
+            lp -= md
+        return lp
+
+    def objf(self, *args, **kwargs):
+        self._model.setFreeParamVals(args[0])
+        return self.logPost(*args[1:], **kwargs)
+
+    def update(self):
+        pass
+        
 
