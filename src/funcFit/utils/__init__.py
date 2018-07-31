189cee49db309ebd975bc7b4b5803e0e462906ac
Stefan <stefan.czesla@hs.uni-hamburg.de>
2018-07-31 18:04:49 +0200

    pr2 for marginal probability of data

--- src/funcFit/utils/__init__.py
+++ src/funcFit/utils/__init__.py
@@ -2,5 +2,5 @@ from PyAstronomy.funcFit import ic
 
 if ic.check["numpy"] and ic.check["scipy"] and ic.check["matplotlib"] and ic.check["matplotlib.pylab"]:
   from .anaMCMCTraces import TraceAnalysis, hpd, quantiles
-  from .bayesFactors import bayesFactors
-  __all__ = ["TraceAnalysis", "bayesFactors", "hpd", "quantiles"]
+  from .bayesFactors import bayesFactors, pD_pr2
+  __all__ = ["TraceAnalysis", "bayesFactors", "hpd", "quantiles", "pD_pr2"]
--- src/funcFit/utils/bayesFactors.py
+++ src/funcFit/utils/bayesFactors.py
@@ -1,8 +1,37 @@
 from __future__ import print_function, division
-import numpy
+import numpy as np
 from PyAstronomy.funcFit.utils import ic
 from PyAstronomy.pyaC import pyaErrors as PE
-import scipy.misc as sm
+import scipy.special as ss
+
+def pD_pr2(logl):
+    """
+    Estimate the marginal probability of the data
+    
+    This function estimates the marginal probability of the data using the harmonic
+    mean of the likelihood (KR95, Eq. 11).
+    
+    The resulting estimate is known to be unstable and is often dominated by extreme points.
+    Although the estimate can be shown to converge to the "correct" value, its distribution
+    does not generally correspond to a Gaussian shrinking in width as the number of points
+    increases (Gaussian central limit theorem). See KR95 for further discussion.
+    
+    KR95: Kass, Robert E., and Adrian E. Raftery. "Bayes Factors." Journal of the American Statistical Association 90, no. 430
+    (1995): 773-95. doi:10.2307/2291091.
+    
+    Parameters
+    ----------
+    logl : array
+        The (natural) logarithm of the likelihood (e.g., obtained from a Markov Chain)
+    
+    Returns
+    -------
+    ln(pD) : floats
+        The (natural) logarithm of the estimation of the marginal
+        probability of the data
+    """
+    return np.log(len(logl)) - ss.logsumexp(-logl)
+
 
 def bayesFactors(model1, model2):
     """
@@ -30,6 +59,6 @@ def bayesFactors(model1, model2):
 
     # Given a number of numbers x1, x2, ... whose logarithms are given by l_i=ln(x_i) etc.,
     # logsum calculates: ln(sum_i exp(l_i)) = ln(sum_i x_i)
-    bf = numpy.exp(sm.logsumexp(-logp1) - numpy.log(len(logp1))
-                   - (sm.logsumexp(-logp2) - numpy.log(len(logp2))))
+    bf = np.exp(ss.logsumexp(-logp1) - np.log(len(logp1))
+                   - (ss.logsumexp(-logp2) - np.log(len(logp2))))
     return bf
