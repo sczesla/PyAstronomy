8dfc9d4e77807ef169e7db82fe952bc1a4195eb0
Stefan <stefan.czesla@hs.uni-hamburg.de>
2018-08-01 18:36:52 +0200

    indentations

--- src/funcFit/utils/__init__.py
+++ src/funcFit/utils/__init__.py
@@ -1,6 +1,6 @@
 from PyAstronomy.funcFit import ic
 
 if ic.check["numpy"] and ic.check["scipy"] and ic.check["matplotlib"] and ic.check["matplotlib.pylab"]:
-  from .anaMCMCTraces import TraceAnalysis, hpd, quantiles
-  from .bayesFactors import bayesFactors, pD_pr2
-  __all__ = ["TraceAnalysis", "bayesFactors", "hpd", "quantiles", "pD_pr2"]
+    from .anaMCMCTraces import TraceAnalysis, hpd, quantiles
+    from .bayesFactors import bayesFactors, pD_pr2
+    __all__ = ["TraceAnalysis", "bayesFactors", "hpd", "quantiles", "pD_pr2"]
