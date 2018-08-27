332991bde82ad66412157c67586d33b2558d03fd
Stefan <stefan.czesla@hs.uni-hamburg.de>
2018-08-27 18:37:03 +0200

    PEP8 for Doppler shift

--- src/pyasl/asl/dopplerShift.py
+++ src/pyasl/asl/dopplerShift.py
@@ -4,16 +4,17 @@ import numpy as np
 from PyAstronomy.pyaC import pyaErrors as PE
 import six.moves as smo
 
+
 def dopplerShift(wvl, flux, v, edgeHandling=None, fillValue=None):
-  """
+    """
     Doppler shift a given spectrum.
-    
+
     A simple algorithm to apply a Doppler shift
     to a spectrum. This function, first, calculates
     the shifted wavelength axis and, second, obtains
     the new, shifted flux array at the old, unshifted
     wavelength points by linearly interpolating.
-    
+
     Due to the shift, some bins at the edge of the
     spectrum cannot be interpolated, because they
     are outside the given input range. The default
@@ -21,23 +22,23 @@ def dopplerShift(wvl, flux, v, edgeHandling=None, fillValue=None):
     values at those points. One can, however, specify
     the `edgeHandling` parameter to choose a different
     handling of these points.
-    
+
     If "firstlast" is specified for `edgeHandling`,
     the out-of-range points at the red or blue edge
     of the spectrum will be filled using the first
     (at the blue edge) and last (at the red edge) valid
     point in the shifted, i.e., the interpolated, spectrum.
-    
+
     If "fillValue" is chosen for edge handling,
     the points under consideration will be filled with
     the value given through the `fillValue` keyword.
-    
+
     .. warning:: Shifting a spectrum using linear
                 interpolation has an effect on the
                 noise of the spectrum. No treatment
                 of such effects is implemented in this
                 function.
-    
+
     Parameters
     ----------
     wvl : array
@@ -52,47 +53,47 @@ def dopplerShift(wvl, flux, v, edgeHandling=None, fillValue=None):
     fillValue : float, optional
         If the "fillValue" is specified as edge handling method,
         the value used to fill the edges of the output spectrum.
-    
+
     Returns
     -------
     nflux : array
         The shifted flux array at the *old* input locations.
     wlprime : array
         The shifted wavelength axis.
-  """
-  # Shifted wavelength axis
-  wlprime = wvl * (1.0 + v/299792.458)
-  
-  fv = np.nan
-  if edgeHandling == "fillValue":
-    if fillValue is None:
-      raise(PE.PyAValError("Fill value not specified", where="pyasl.dopplerShift", \
-                           solution="If you request 'fillValue' as edge handling method, you need to specify the 'fillValue' keyword."))
-    fv = fillValue
-      
-  f = sci.interp1d(wlprime, flux, bounds_error=False, fill_value=fv)
-  nflux = f(wvl)
-  
-  if edgeHandling == "firstlast":
-    firsts = []
-    # Search for first non-NaN value save indices of
-    # leading NaN values
-    for i in smo.range(len(nflux)):
-      if np.isnan(nflux[i]):
-        firsts.append(i)
-      else:
-        firstval = nflux[i]
-        break
-    # Do the same for trailing NaNs
-    lasts = []
-    for i in smo.range(len(nflux)-1,0,-1):
-      if np.isnan(nflux[i]):
-        lasts.append(i)
-      else:
-        lastval = nflux[i]
-        break
-    # Use first and last non-NaN value to
-    # fill the nflux array
-    nflux[firsts] = firstval
-    nflux[lasts] = lastval
-  return nflux, wlprime
+    """
+    # Shifted wavelength axis
+    wlprime = wvl * (1.0 + v / 299792.458)
+
+    fv = np.nan
+    if edgeHandling == "fillValue":
+        if fillValue is None:
+            raise(PE.PyAValError("Fill value not specified", where="pyasl.dopplerShift",
+                                 solution="If you request 'fillValue' as edge handling method, you need to specify the 'fillValue' keyword."))
+        fv = fillValue
+
+    f = sci.interp1d(wlprime, flux, bounds_error=False, fill_value=fv)
+    nflux = f(wvl)
+
+    if edgeHandling == "firstlast":
+        firsts = []
+        # Search for first non-NaN value save indices of
+        # leading NaN values
+        for i in smo.range(len(nflux)):
+            if np.isnan(nflux[i]):
+                firsts.append(i)
+            else:
+                firstval = nflux[i]
+                break
+        # Do the same for trailing NaNs
+        lasts = []
+        for i in smo.range(len(nflux) - 1, 0, -1):
+            if np.isnan(nflux[i]):
+                lasts.append(i)
+            else:
+                lastval = nflux[i]
+                break
+        # Use first and last non-NaN value to
+        # fill the nflux array
+        nflux[firsts] = firstval
+        nflux[lasts] = lastval
+    return nflux, wlprime
