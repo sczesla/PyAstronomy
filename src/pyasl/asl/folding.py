1b9b6e683b12630fdb2c59d3d6608387ceb032e4
Stefan <stefan.czesla@hs.uni-hamburg.de>
2018-12-10 14:24:56 +0100

    PEP8 for folding

--- src/pyasl/asl/folding.py
+++ src/pyasl/asl/folding.py
@@ -1,12 +1,13 @@
 import numpy
 
+
 def foldAt(time, period, T0=0.0, getEpoch=False):
-  """
+    """
     Fold time series at a particular period.
-    
+
     In this function, the phase is a number between 0 and 1
     (and not between 0 and 2pi).
-    
+
     Parameters
     ----------
     time : array
@@ -19,7 +20,7 @@ def foldAt(time, period, T0=0.0, getEpoch=False):
         If True, an array holding the epoch for every point
         in time will be returned; the default is False.
         Note that the first epoch is 0.
-  
+
     Returns
     -------
     Phases : array
@@ -28,9 +29,9 @@ def foldAt(time, period, T0=0.0, getEpoch=False):
         An array holding the epoch for every given point in time.
         The counting starts at zero. Only returned if `getEpoch`
         is True.
-  """
-  epoch = numpy.floor( (time - T0)/period )
-  phase = (time - T0)/period - epoch
-  if getEpoch:
-    return phase, epoch
-  return phase
+    """
+    epoch = numpy.floor((time - T0) / period)
+    phase = (time - T0) / period - epoch
+    if getEpoch:
+        return phase, epoch
+    return phase
