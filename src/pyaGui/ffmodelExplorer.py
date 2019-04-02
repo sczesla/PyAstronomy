b27f74b834d7f4fff981f4dd0369a83f9ef0284d
Stefan <stefan.czesla@hs.uni-hamburg.de>
2019-04-02 15:29:34 +0200

    Account for renaming of NavigationToolbar2TkAgg

--- src/pyaGui/ffmodelExplorer.py
+++ src/pyaGui/ffmodelExplorer.py
@@ -2,7 +2,11 @@
 from __future__ import print_function, division
 import matplotlib
 
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+try:
+    from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg
+except ImportError:
+    from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk as NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 from PyAstronomy.pyaC import pyaErrors as PE
 import numpy
