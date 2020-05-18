6a209fe1cf8d6da5bab0c6447977bda7fad930b0
Stefan Czesla <stefan.czesla@hs.uni-hamburg.de>
2020-05-18 21:36:44 +0200

    PEP8 for sweet_cat.py

--- src/pyasl/resBased/sweet_cat.py
+++ src/pyasl/resBased/sweet_cat.py
@@ -8,7 +8,7 @@ import ssl
 
 
 class SWEETCat(pp.PyAUpdateCycle):
-  """
+    """
     Access the SWEET-Cat catalog.
 
     The SWEET-Cat catalog provides parameters for planet host stars.
@@ -57,65 +57,68 @@ class SWEETCat(pp.PyAUpdateCycle):
     data : pandas data frame
         The catalog data
 
-  """
-
-
-  def _downloadData(self):
     """
-      Download SWEETCAT and write it to file
-    """
-    url = 'https://www.astro.up.pt/resources/sweet-cat/download.php'
-    dfn = self._fs.composeFilename(self.dataFileName)
-    try:
-      # The next two lines by-pass certificate verification
-      context = ssl._create_unverified_context()
-      self._fs.downloadToFile(url, dfn, clobber=True, verbose=False, openMethod=gzip.open, \
-                              context=context)
-    except AttributeError:
-      # Python version does not support ssl._create_unverified_context()
-      self._fs.downloadToFile(url, dfn, clobber=True, verbose=False, openMethod=gzip.open)
-    except Exception as e:
-      # "Handle" unexpected error
-      raise(PE.PyANetworkError("Could not download SWEET-Cat data. The following error was raised: " + str(e)))
 
-  def _read_sweetcat(self):
-    """
-      Read SWEETCat into a pandas DataFrame
-    """
-    if not _ic.check["pandas"]:
-      raise(PE.PyARequiredImport("Could not import pandas module.", \
-                                  solution="Please install pandas (http://pandas.pydata.org). Please also check the dependencies."))
-    else:
-      import pandas as pd
-    ffn = self._fs.requestFile(self.dataFileName, 'r', gzip.open)
-    self.data = pd.read_csv(ffn, sep='\t', names=self.names, na_values=['~'])
+    def _downloadData(self):
+        """
+        Download SWEETCAT and write it to file
+        """
+        url = 'https://www.astro.up.pt/resources/sweet-cat/download.php'
+        dfn = self._fs.composeFilename(self.dataFileName)
+        try:
+            # The next two lines by-pass certificate verification
+            context = ssl._create_unverified_context()
+            self._fs.downloadToFile(url, dfn, clobber=True, verbose=False, openMethod=gzip.open,
+                                    context=context)
+        except AttributeError:
+            # Python version does not support ssl._create_unverified_context()
+            self._fs.downloadToFile(
+                url, dfn, clobber=True, verbose=False, openMethod=gzip.open)
+        except Exception as e:
+            # "Handle" unexpected error
+            raise(PE.PyANetworkError(
+                "Could not download SWEET-Cat data. The following error was raised: " + str(e)))
 
-  def __init__(self, skipUpdate=False):
-    self.dataFileName = os.path.join("pyasl", "resBased", "sweetcat.csv.gz")
-    configFilename = os.path.join("pyasl", "resBased", "sweetcat.cfg")
-    pp.PyAUpdateCycle.__init__(self, configFilename, "sweetcatupdate")
+    def _read_sweetcat(self):
+        """
+        Read SWEETCat into a pandas DataFrame
+        """
+        if not _ic.check["pandas"]:
+            raise(PE.PyARequiredImport("Could not import pandas module.",
+                                       solution="Please install pandas (http://pandas.pydata.org). Please also check the dependencies."))
+        else:
+            import pandas as pd
+        ffn = self._fs.requestFile(self.dataFileName, 'r', gzip.open)
+        self.data = pd.read_csv(
+            ffn, sep='\t', names=self.names, na_values=['~'])
 
-    self.names = ['star', 'hd', 'ra', 'dec', 'vmag', 'ervmag', 'par', 'erpar',
-             'parsource', 'teff', 'erteff', 'logg', 'erlogg', 'logglc',
-             'erlogglc', 'vt', 'ervt', 'metal', 'ermetal', 'mass', 'ermass',
-             'author', 'source', 'update', 'comment']
+    def __init__(self, skipUpdate=False):
+        self.dataFileName = os.path.join(
+            "pyasl", "resBased", "sweetcat.csv.gz")
+        configFilename = os.path.join("pyasl", "resBased", "sweetcat.cfg")
+        pp.PyAUpdateCycle.__init__(self, configFilename, "sweetcatupdate")
 
-    # Check whether data file exists
-    self._fs = pp.PyAFS()
-    if (self.needsUpdate() or (not self._fs.fileExists(self.dataFileName))) and (not skipUpdate) :
-      # Data needs update
-      print("Downloading exoplanet data from SWEET-Cat archive")
-      self._update(self._downloadData)
-      print("Saved data to file: ", self.dataFileName, " in data directory,")
-      print("  which has been configured as: ", self._fs.dpath)
-      print("By default, the data will be downloaded anew every 7 days.")
-      print("You can use the `changeDownloadCycle` to change this behavior.")
-    self._read_sweetcat()
+        self.names = ['star', 'hd', 'ra', 'dec', 'vmag', 'ervmag', 'par', 'erpar',
+                      'parsource', 'teff', 'erteff', 'logg', 'erlogg', 'logglc',
+                      'erlogglc', 'vt', 'ervt', 'metal', 'ermetal', 'mass', 'ermass',
+                      'author', 'source', 'update', 'comment']
 
+        # Check whether data file exists
+        self._fs = pp.PyAFS()
+        if (self.needsUpdate() or (not self._fs.fileExists(self.dataFileName))) and (not skipUpdate):
+            # Data needs update
+            print("Downloading exoplanet data from SWEET-Cat archive")
+            self._update(self._downloadData)
+            print("Saved data to file: ",
+                  self.dataFileName, " in data directory,")
+            print("  which has been configured as: ", self._fs.dpath)
+            print("By default, the data will be downloaded anew every 7 days.")
+            print("You can use the `changeDownloadCycle` to change this behavior.")
+        self._read_sweetcat()
 
-  def downloadData(self):
-    """
-      Trigger download of data.
-    """
-    self._update(self._downloadData)
-    self._read_sweetcat()
+    def downloadData(self):
+        """
+        Trigger download of data.
+        """
+        self._update(self._downloadData)
+        self._read_sweetcat()
