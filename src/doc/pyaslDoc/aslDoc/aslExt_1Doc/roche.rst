c2fcc2be067a93a81eb3a4b22d546b129fc99950
Stefan <stefan.czesla@hs.uni-hamburg.de>
2020-07-03 14:46:21 +0200

    Roche doku

--- src/doc/pyaslDoc/aslDoc/aslExt_1Doc/roche.rst
+++ src/doc/pyaslDoc/aslDoc/aslExt_1Doc/roche.rst
@@ -1,8 +1,49 @@
 Roche potential
-------------------------
+======================
 
 .. p23ready
 
 .. currentmodule:: PyAstronomy.pyasl
 
+Let there be two masses :math:`m_1` and :math:`m_2` with :math:`m_1 \ge m_2` and
+:math:`m_1` centered at :math:`(x,y,z) = (0,0,0)` and :math:`(x,y,z) = (a,0,0)`, where
+:math:`a` is the separation between the masses.
+Both objects are located in the x,y plane with x-axis counting distance along the
+connecting line and z measuring distance perpendicular to the orbital plane.
+
+For synchronous rotation and a circular orbit, the Roche potential
+reads (e.g., Hilditch 2001 "An introduction to close binary stars")
+
+.. math::
+
+    \Phi &= -\frac{G m_1}{r_1} - \frac{G m_2}{r_2} - \frac{\omega^2}{2}\left[\left(x - a\frac{m_2}{m_1+m_2}\right)^2 + y^2 \right] \\
+
+with
+
+.. math::    
+
+    r_1 &= \sqrt{x^2 + y^2 + z^2} \\
+    r_2 &= \sqrt{(x-a)^2 + y^2 + z^2} \\
+    q &= \frac{m_2}{m_1} \\
+    M &= m_1 + m_2 \\
+    m_1 &= \frac{M}{1+q} \\
+    m_2 &= \frac{Mq}{1+q} \\
+    \omega^2 &= \frac{GM}{a^3} \;\;\; \mbox{(Kepler's third law)} \\
+    x', y', z', r_1', r_2' &= \frac{x}{a}, \frac{y}{a}, \frac{z}{a}, \frac{r_1}{a}, \frac{r_2}{a}
+    
+we write
+
+.. math::
+
+    \Phi &= -\frac{G M}{(1+q) r_1} - \frac{G M q}{(1+q) r_2} - \frac{GM}{2 a^3}\left[\left(x - \frac{a\,q}{1+q}\right)^2 + y^2 \right] \\
+    \Phi &= -\frac{G M}{(1+q) r_1'\,a} - \frac{G M q}{(1+q) r_2'\,a} - \frac{GM}{2 a^3}\left[\left(x'\,a - \frac{a\,q}{1+q}\right)^2 + y'^2\,a^2 \right] \\
+    \Phi &= -\frac{GM}{2 a} \left( \frac{2}{(1+q) r_1'} + \frac{2 q}{(1+q) r_2'} + \left(x' - \frac{q}{1+q}\right)^2 + y'^2   \right) \\
+    \Phi &= -\frac{GM}{2 a} \; \Phi_n(x', y', z', q)
+    
+where :math:`\Phi_n` is the dimensionless Roche potential.
+
+Functions to obtain the potential
+-----------------------------------
+
 .. autofunction:: rochepot_dl
+.. autofunction:: rochepot
--- src/pyasl/asl/aslExt_1/roche.py
+++ src/pyasl/asl/aslExt_1/roche.py
@@ -1,20 +1,15 @@
 import numpy as np
 from PyAstronomy import constants as PC
+from PyAstronomy.pyaC import pyaErrors as PE
 
 def rochepot_dl(x, y, z, q):
     """
-    Dimensionless Roche potential
+    Dimensionless Roche potential (:math:`\\Phi_n`, synchronous rotation)
     
-    More massive component (m1) is at (x,y,z) = (0,0,0). Less massive
-    component (m2) is at (1,0,0). The unit of length is the distance between
-    the objects. Both objects are in the x,y plane. The dimensionless Roche
-    potential is given by
-    
-    .. math::
-    
-        \\Phi_n &= \\frac{2}{(1+q) r_1} + \\frac{2 q}{(1+q) r_2} + \\left(x - \\frac{q}{1+q}\\right)^2 + y^2 \\\\
-        r_1 &= \\sqrt{x^2 + y^2 + z^2} \\\\
-        r_2 &= \\sqrt{(x-1)^2 + y^2 + z^2}
+    More massive component (:math:`m_1`) is centered at (x,y,z) = (0,0,0). Less massive
+    component (:math:`m_2`) is at (1,0,0). The unit of length is the distance between
+    the objects. Both objects are in the x,y plane (x-axis along the connecting line and
+    z perpendicular to the orbital plane). 
     
     Parameters
     ----------
@@ -22,13 +17,17 @@ def rochepot_dl(x, y, z, q):
         Location(s) at which to calculate the potential. Unit
         of length is the distance between the masses m1 and m2.
     q : float
-        Mass ratio (m2/m1 <= 1)
+        Mass ratio (0 <= m2/m1 <= 1)
     
     Returns
     -------
     Potential : float or array
         The potential at the specified location(s)
     """
+    if (q < 0) or (q > 1):
+        raise(PE.PyAValError("Invalid mass ratio q=m2/m1 of '" + str(q) + "'.", \
+                             where="rochepot_dl", \
+                             solution="Check masses. Perhaps exchange m1 and m2 so that m1 >= m2."))
     r1 = np.sqrt(x**2 + y**2 + z**2)
     r2 = np.sqrt((x-1)**2 + y**2 + z**2)
     p = 2/((1+q)*r1) + 2*q/((1+q)*r2) + (x - q/(1+q))**2 + y**2
@@ -37,11 +36,10 @@ def rochepot_dl(x, y, z, q):
 
 def rochepot(x, y, z, m1, m2, a):
     """
-    Roche potential
+    Roche potential (:math:`\\Phi`, synchronous rotation)
     
-    More massive component (m1) is at (x,y,z) = (0,0,0). Less massive
-    component (m2) is at (a,0,0). Both objects are in the x,y plane.
-    The Roche potential is given by
+    More massive component (:math:`m_1`) is at (x,y,z) = (0,0,0). Less massive
+    component (:math:`m_2`) is at (a,0,0).
     
     Parameters
     ----------
@@ -58,8 +56,8 @@ def rochepot(x, y, z, m1, m2, a):
         At specified locations [m**2/s**2]
     """
     # Gravitational constant (m**3/(kg*s**2))
-    G = 6.67430
+    G = 6.67430e-11
     M = m1 + m2
-    return -(G*M)/(2*a) * rochepot_dl(x/a, y/a, z/a, m2/m2)
+    return -(G*M)/(2*a) * rochepot_dl(x/a, y/a, z/a, m2/m1)
     
     
