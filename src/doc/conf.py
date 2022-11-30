854bc8fad6ec83da103977ecc0e63c93b296a970
Stefan Czesla <stefan.czesla@hs.uni-hamburg.de>
2022-11-30 21:53:49 +0100

    unmock copy

--- src/doc/conf.py
+++ src/doc/conf.py
@@ -8,7 +8,7 @@ import mock
 MOCK_MODULES = ['scipy', 'scipy.stats', 'scipy.special', 'scipy.optimize', 'scipy.interpolate', 'scipy.integrate', 'scipy.misc',
                 'pymc', 'matplotlib', 'emcee',
                 'matplotlib.pylab', '_tkinter', 'Tkinter', 'matplotlib.backends', 'matplotlib.backends.backend_tkagg',
-                'matplotlib.figure', 'astropy', 'astropy.table', 'astropy.io', 'astropy.io.fits', 'copy']
+                'matplotlib.figure', 'astropy', 'astropy.table', 'astropy.io', 'astropy.io.fits']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
