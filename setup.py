b197d13e0b55027a3c070793002d8dd15bc2f04a
Stefan Czesla <kontakt@sczesla.eu>
2024-01-11 21:48:06 +0100

    Specifying Numpy<2 for the moment

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ setup(cmdclass={"with-ext": WithExtCommand},
       packages=packages,
       ext_modules=ext_modules,
       package_dir={'PyAstronomy': 'src'},
-      install_requires=['numpy', 'six', 'scipy', 'quantities', 'bidict'],
+      install_requires=['numpy<2', 'six', 'scipy', 'quantities', 'bidict'],
       extras_require={"occult":"PyAstronomy_ext"},
       # Do not forget to give the ``correct'' name for the module! (here, e.g., PyAstronomy.funcFit)
       package_data=package_data,
